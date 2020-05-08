222cf332124853836bd00abceab2d145653ee633
Shreyan Ganguly <52214843+GangulyShreyan@users.noreply.github.com>
2020-05-08 14:12:40 +0530

    Update core.py

--- core.py
+++ core.py
@@ -1,7 +1,7 @@
 from PIL import Image
 def run(link,n, imageid):
     img = Image.open(link)
-    imgSmall = img.resize((50,50), resample = Image.ANTIALIAS)
+    imgSmall = img.resize((n,n), resample = Image.ANTIALIAS)
     finalimg = imgSmall.resize(img.size, Image.NEAREST)
     outputfilename = "output" + imageid + ".png"
     finalimg.save("static/output/" + outputfilename,"PNG")
